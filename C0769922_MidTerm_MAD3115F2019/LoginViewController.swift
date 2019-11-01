28538b82b04e7179fd8bdf7c21f000ff228a17df
Jeenapaul <jeenapaul1014@gmail.com>
2019-11-01 14:56:52 -0400

    Title Added

--- C0769922_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0769922_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -20,21 +20,29 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Login(_ sender: UIButton) {
+  
+    
+    @IBAction func Login(_ sender: UIBarButtonItem) {
         self.validateUser()
     }
+    
+        
+
+    @IBAction func RememberMe(_ sender: UISwitch) {
+    }
+
     func validateUser()
         
     {
         
-        if(EmailID.text == "jeenapaul1014@gmail.com" && Password.text == "jeenapaul")
-     
+        if(EmailID.text == "jeenapaul1014@gmail.com" && Password.text == "jeena987")
             
         {
             
             let myStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             
             let nextVC = myStoryBoard.instantiateViewController(withIdentifier: "CustomerVC") as! CustomerListTableViewController
+            
             self.present(nextVC, animated: true, completion: nil)
             
             
@@ -42,14 +50,16 @@ class LoginViewController: UIViewController {
         }
             
         else
+            
         {
             
-            let alert  =
-                
-                UIAlertController(title: "Error", message: "User Email / Password incorrect", preferredStyle: UIAlertController.Style.alert)
+            let alert  = UIAlertController(title: "Error", message: "User Email / Password incorrect", preferredStyle: UIAlertController.Style.alert)
+        let actionOk = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             
-            //UIAlertControlle        @IBAction func RememberMe(_ sender: UISwitch) {
-    }
-    
+            alert.addAction(actionOk)
+            self.present(alert, animated: true, completion: nil)
+            
 }
 
+}
+}
